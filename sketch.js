d96d451e3f0dee9a1a8db0495bb19d1d29a21573
MatildeMammana <matilde.mammana@gmail.com>
2018-10-09 21:28:00 +0200

    Texture completed

--- sketch.js
+++ sketch.js
@@ -3,9 +3,35 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight)
+  background(245, 245, 245)
 }
 
 function draw() {
-  // put drawing code here
+
+  if(random()<0.1)
+  {
+   fill(255, 87, 51, 100);
+  } else {
+   fill(249, 231, 159);
+  }
+      for(var y = 0 ; y < windowHeight; y+=50)
+      {
+        noStroke();
+        ellipse(random()* width, y, 70, 90);
+      }
+
+  if(random()<0.1)
+{
+   fill(192, 57, 43);
+} else
+{
+   fill(127, 179, 213, 100);
 }
+
+  for(var x = 0 ; x < windowWidth ; x+= 50)
+  {
+    noStroke();
+    ellipse(x, random()* height, 70, 90);
+}
+  }
